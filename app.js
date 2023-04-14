003f1c51e61fc379650fea6acc9a9c464f5a413f
Egor <pihtyarevv@mail.ru>
2023-04-14 09:59:25 +0300

    Корзина товаров
    Работа с prototype

--- app.js
+++ app.js
@@ -1,19 +1,69 @@
 "use strict";
 
-function pizzaTimer(ms) {
-  const deadLine = new Date().getTime() + ms;
-  const interval = setInterval(() => {
-    console.log(
-      new Intl.DateTimeFormat("ru-RU", {
-        minute: "numeric",
-        second: "numeric",
-      }).format(deadLine + 150 - new Date())
-    );
-  }, 1000);
-  setTimeout(() => {
-    clearInterval(interval);
-    console.log("PIZZA Done");
-  }, ms);
-}
+const product = { id: 1, name: "Bread", count: 1 };
 
-pizzaTimer(5000);
+// МОЕ РЕШЕНИЕ
+
+// const Cart = function (id, name, count) {
+//   this.id = id;
+//   this.name = name;
+//   this.count = count;
+// };
+
+// Cart.prototype.addCountProduct = function (count) {
+//   this.count += count;
+// };
+
+// Cart.prototype.removeCountProduct = function (count) {
+//   this.count -= count;
+//   if (this.count == 0 || this.count < 0) {
+//     this.count = "Товаров нет";
+//   }
+// };
+
+// const product2 = new Cart(2, "Nosok", 2);
+// product2.removeCountProduct(1);
+// product2.addCountProduct(10);
+
+// console.log(product2);
+
+//РЕШЕНИЕ ПРЕПОДАВАТЕЛЯ
+
+const Cart = function () {
+  this.products = [];
+};
+
+Cart.prototype.addProduct = function (product) {
+  if (this.products.find((product) => product.id === product.id)) {
+    return;
+  }
+  this.products.push(product);
+};
+
+Cart.prototype.increaseAmount = function (id) {
+  this.products = this.products.map((product) => {
+    if (product.id == id) {
+      product.count++;
+      return product;
+    }
+    return product;
+  });
+};
+
+Cart.prototype.decreaseAmount = function (id) {
+  this.products = this.products
+    .map((product) => {
+      if (product.id == id) {
+        product.count--;
+        return product;
+      }
+      return product;
+    })
+    .filter((product) => product.count > 0);
+};
+
+const cart = new Cart();
+cart.addProduct(product);
+cart.increaseAmount(1);
+cart.decreaseAmount(1);
+console.log(cart);
