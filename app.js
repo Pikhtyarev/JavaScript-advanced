073f73a539f6539e3073747bb2313ab000688bb4
Egor <pihtyarevv@mail.ru>
2023-04-19 10:08:22 +0300

    Расчет цены
    Работа с DummyJSON, отправка HTTP запроса

--- app.js
+++ app.js
@@ -1,42 +1,12 @@
 "use strict";
 
-class Enemy {
-  health;
-  constructor(health) {
-    this.health = health;
-  }
+const request = new XMLHttpRequest();
+request.open("GET", "https://dummyjson.com/products");
+request.send();
 
-  getDamage(damage) {
-    this.health -= damage;
-  }
-}
-
-class Orc extends Enemy {
-  constructor(health) {
-    super(health);
-  }
-
-  getDamage(damage) {
-    if (Math.random() > 0.5) {
-      return this.health -= damage;
-    } else return this.health;
-  }
-}
-
-class Sword {
-  #power;
-  constructor(power) {
-    this.#power = power;
-  }
-
-  giveDamage(enemy) {
-    enemy.getDamage(this.#power);
-  }
-}
-
-const orc = new Orc(100);
-
-const mech = new Sword(50);
-mech.giveDamage(orc);
-
-console.log(orc);
+request.addEventListener("load", function() {
+  const { products } = JSON.parse(this.responseText);
+  console.log(products);
+  const sum = products.reduce((acc, product) => acc += product.price, 0);
+  console.log(sum / products.length);
+});
