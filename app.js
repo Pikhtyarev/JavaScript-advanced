c574730669ab59592acea7be49f960da7848f9be
Egor <pihtyarevv@mail.ru>
2023-04-22 08:54:13 +0300

    Select выбора категорий
    Внедрение в HTML селекта

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 "use strict";
 
-const request = new XMLHttpRequest();
-request.open("GET", "https://dummyjson.com/products");
-request.send();
+function createSelect(array) {
+  const el = document.getElementById("block");
+  el.innerHTML = `<select>
+    ${array.map(arrEl => `<option value=${arrEl}>${arrEl}</option>`)}
+  </select>`
+}
 
-request.addEventListener("load", function() {
-  const { products } = JSON.parse(this.responseText);
-  console.log(products);
-  const sum = products.reduce((acc, product) => acc += product.price, 0);
-  console.log(sum / products.length);
-});
+function getCategories() {
+  fetch("https://dummyjson.com/products/categories")
+  .then(response => response.json())
+  .then(data => createSelect(data))
+  .catch(console.error(`Ошибка: ${error}`))
+}
+
+getCategories();
