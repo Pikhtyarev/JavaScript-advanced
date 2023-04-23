e5ff2b11a5674491f2509e0e569c19efac62dd10
Egor <pihtyarevv@mail.ru>
2023-04-23 09:19:26 +0300

    Мой город
    Работа с API geolocation + bigdatacloud/reverse-geocode-client, с использованием async await

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
 "use strict";
 
-function getData(url, errorMessage) {
-  return fetch(url)
-    .then(response => {
-        if (!response.ok) {
-          throw new Error(`${errorMessage}: ${response.status}`)
-        }
-        return response.json()
-    })
+// МОЕ РЕШЕНИЕ
+
+// function success(pos) {
+//   let crd = pos.coords;
+//   const latitude = crd.latitude;
+//   const longitude = crd.longitude;
+// }
+
+// function error(err) {
+//   console.error(err);
+// }
+
+// navigator.geolocation.getCurrentPosition(success, error);
+
+
+// async function getLocation() {
+//   const resolve = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${success.latitude}&longitude=${success.longitude}`);
+//   const result = await resolve.json();
+
+//   console.log(result.city);
+// }
+
+// getLocation();
+
+
+
+
+// РЕШЕНИЕ ПРЕПОДАваТЕЛЯ
+
+function getMyCoordinats() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        resolve({ latitude: coords.latitude, longitude: coords.longitude, })
+      }, (err) => {
+        reject(err);
+      }
+    );
+  })
 }
 
-getData("https://dummyjson.com/products", "OSHIBKA")
-  .then(({ products }) => {
-    return getData("https://dummyjson.com/products/" + products[0].id, "OSHIBKA 2")
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    const el = document.getElementById("block");
-    el.innerHTML = error.message;
-  });
+async function getMyCity() {
+  try {
+    const { latitude, longitude } = await getMyCoordinats();
+    const response = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`);
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    const data = await response.json();
+    console.log(data.city);
+  } catch(e) {
+    console.error(e);
+  }
+}
+
+getMyCity();
